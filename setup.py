c9db77b7a58db902cb0350a8898937596c7693c0
SMB H <smbhop@pm.me>
2023-03-06 13:34:36 +0330

    :construction: new(project): Init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='har',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing heart data via association rules',
+    author='SMB H',
+    license='BSD-3',
+)
